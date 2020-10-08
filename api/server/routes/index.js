0c0c1bbb9b5df375b7ff6f5714feded11e36d993
Christopher Richa <christopherricha90@gmail.com>
2020-10-07 21:14:56 -0400

    Initial implementation nedb.

--- api/server/routes/index.js
+++ api/server/routes/index.js
@@ -1,10 +1,28 @@
 import express from 'express';
+import Datasource from 'nedb';
+import { register } from 'babel-core';
+import babelPolyfill from 'babel-polyfill';
 
+const db = new Datasource();
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World' });
+router.get('/', async (req, res) => {
+  db.find(req.query).exec((err, docs) => {
+    if (err) {
+      res.send({ err });
+    } else {
+      res.send({ docs });
+    }
+  });
+});
+
+router.post('/', function (req, res, next) {
+  db.insert(req.body, (err, docs) => {
+    if (err) {
+      res.send("error");
+    }
+    res.send(docs)
+  });
 });
 
 export default router;
